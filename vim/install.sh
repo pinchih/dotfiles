39e321a7794a3164dc569c76e26be0ea7492a8fe
Pin-Chih Lin <tobeaceo@gmail.com>
2020-06-30 20:03:22 -0500

    Update install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -1,8 +1,5 @@
-#!/usr/bin/env bas
+#!/usr/bin/env bash
 
-# Copy vimrc.txt to .vimrc
-echo "Copying vimrc.txt to .vimrc..."
-cp vimrc.txt .vimrc
 echo "Copying .vimrc to ~/.vimrc..."
 mv .vimrc ~/.vimrc
 echo "Done"
